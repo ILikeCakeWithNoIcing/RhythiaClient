1ab65d682ab6b9a96f47f8ea394c0900564eff88
nyuwaea <luniwae@gmail.com>
2026-09-21 17:35:32 -0400

    set version label on ready

---
+++ scripts/ui/menu/VersionLabel.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+using Godot;
+
+public partial class VersionLabel : RichTextLabel
+{
+	public override void _Ready()
+	{
+        string version = (string)ProjectSettings.GetSetting("application/config/version");
+
+        Text = string.Format(CultureInfo.CurrentCulture, Text, version);
+	}
+}
